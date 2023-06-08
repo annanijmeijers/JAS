d4cea3e86797fac9bdc1ea7b64e6fca6aff362b4
sebastiaan-ai-23 <sebassmash08@gmail.com>
2023-06-08 11:45:12 +0200

    implemented route method for route.py

--- code/classes/route.py
+++ code/classes/route.py
@@ -1,35 +1,44 @@
-from .route import Station 
-
+from .station import Station 
+import pandas
 class Route(): 
 
-    def __init__(self, timeframe): 
-
+    def __init__(self, timeframe, df): 
         # needs to contain a variable that keeps track of the route, 
         # and the total duration of the route
         self.route = []
         self.timeframe = timeframe
         self.duration = 0
-
-        pass 
+        self.df = df
 
     def initialise_route(self): 
-
         # get a starting station from dataframe
         # can be random or picked with a specific constraint
-        if self.route == []:
-            starting_station = Station() # 
-            # self.route.append() starting station name
-
-        pass
+        
+        starting_station = Station(self.df["station1"].sample()) 
+        self.route.append(starting_station)
 
     def add_connection(self): 
+        # create instance for current station
+        current_station = self.route[-1]
 
         # check possible connections choose first available one
-        connections = Station(self.route[-1]).connections
+        connections = Station(current_station).connections
         for connection_name, time in connections.items():
             if (Station(connection_name).passed == False) and (self.timeframe - time > 0):
                 self.route.append(connection_name)
                 Station(connection_name).passed = True
                 self.duration += time
                 self.timeframe -= time
-        pass 
+        
+        # check if the current station is the last station
+        if current_station == self.route[-1]:
+            pass # doorgeven dat de route klaar is
+
+    def route(self):
+        #  makes a route within the timeframe
+        if self.route == []:
+            self.initialise_route()
+
+        else:
+            if self.timeframe > 0: # checken of dit niet aan t begin moet
+                self.add_connection()
