ec9480e7b97e3fd7adcb273ff3d72fec1ff41091
sebastiaan-ai-23 <sebassmash08@gmail.com>
2023-06-08 19:55:38 +0200

    route.py niet gecontroleerd wel uitleg comments

--- code/classes/route.py
+++ code/classes/route.py
@@ -19,16 +19,21 @@ class Route():
 
     def add_connection(self): 
         # create instance for current station
-        current_station = self.route[-1]
+        current_station = self.route[-1] # chooses the last station in the route station objects
 
-        # check possible connections choose first available one
-        connections = current_station.connections
-        random_connection = random.choice(list(connections.keys()))
+        # choose random connection
+        connections = current_station.connections # gives connections dictionary
+        random_connection = random.choice(list(connections.keys())) # picks a random dict key as connection
+
+        # increase duration of route and decrease the total timeframe
         self.duration += connections[random_connection]
-        for station in set(self.list_of_stations):
-            if station.station_name == random_connection:
+        self.timeframe -= connections[random_connection]
+
+        for station in self.list_of_stations: # is dit handig via een set?
+            if station.name == random_connection: # searches for the station object of the random connection station
                 self.route.append(station)
 
+        # wel de keuzes binnen het tijdframe houden 
         '''       
         for connection_name, time in connections.items():
             if (Station(connection_name).passed == False) and (self.timeframe - time > 0):
@@ -36,16 +41,14 @@ class Route():
                 Station(connection_name).passed = True
                 self.duration += time
                 self.timeframe -= time
-        ''' 
+         
         # check if the current station is the last station
         if current_station == self.route[-1]:
             pass # doorgeven dat de route klaar is
-
-    def route(self):
+        '''
+    def compute_route(self):
         #  makes a route within the timeframe
-        if self.route == []:
-            self.initialise_route()
+        self.initialise_route()
 
-        else:
-            if self.timeframe > 0: # checken of dit niet aan t begin moet
-                self.add_connection()
+        while self.timeframe > 0: # checken of dit niet aan t begin moet
+            self.add_connection()
