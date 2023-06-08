0864587ca3e303406490a6a4f915cfffc0e8c7cf
sebastiaan-ai-23 <sebassmash08@gmail.com>
2023-06-08 14:32:23 +0200

    route.py baseline start

--- code/classes/route.py
+++ code/classes/route.py
@@ -1,20 +1,20 @@
 from .station import Station 
-import pandas
+import random
 class Route(): 
 
-    def __init__(self, timeframe, df): 
+    def __init__(self, timeframe, list_of_stations): 
         # needs to contain a variable that keeps track of the route, 
         # and the total duration of the route
         self.route = []
         self.timeframe = timeframe
         self.duration = 0
-        self.df = df
+        self.list_of_stations = list_of_stations
 
     def initialise_route(self): 
         # get a starting station from dataframe
         # can be random or picked with a specific constraint
         
-        starting_station = Station(self.df["station1"].sample()) 
+        starting_station = random.choice(self.list_of_stations)
         self.route.append(starting_station)
 
     def add_connection(self): 
@@ -22,14 +22,21 @@ class Route():
         current_station = self.route[-1]
 
         # check possible connections choose first available one
-        connections = Station(current_station).connections
+        connections = current_station.connections
+        random_connection = random.choice(list(connections.keys()))
+        self.duration += connections[random_connection]
+        for station in set(self.list_of_stations):
+            if station.station_name == random_connection:
+                self.route.append(station)
+
+        '''       
         for connection_name, time in connections.items():
             if (Station(connection_name).passed == False) and (self.timeframe - time > 0):
                 self.route.append(connection_name)
                 Station(connection_name).passed = True
                 self.duration += time
                 self.timeframe -= time
-        
+        ''' 
         # check if the current station is the last station
         if current_station == self.route[-1]:
             pass # doorgeven dat de route klaar is
