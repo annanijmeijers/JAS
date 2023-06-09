6d4e55c76c95a95ae913ee836a5e9b7ac3a5e57a
Jensvanderweide <jensvanderweide1@gmail.com>
2023-06-09 16:20:27 +0200

    Please enter the commit message for your changes. Lines starting

--- code/classes/network.py
+++ code/classes/network.py
@@ -2,41 +2,49 @@ from .route import Route
 
 class Network(): 
 
-    def __init__(self, dataframe, routes): 
-
-        self.p = None 
-        self.routes = routes 
-        self.min = None 
+    def __init__(self):         
 
         #  store the different routes 
         self.routes = []
 
         # storing all the covered tracks
-        self.tracks = []
+        self.covered_tracks = set()
 
-        pass 
+        # storing the total duration of all routes 
+        self.total_duration = None 
+    
+        self.total_tracks = None 
 
-    def add_route(self):
-        pass
+        self.route_number = None 
 
-    def covered_tracks(self): 
+    def add_route(self, route_list, route_tracks, route_duration):
         """
-        Calculates the fraction of covered tracks: p
+        Add the route to a list, add the set of covered tracks for this route
+        to the total set of covered tracks 
         """
-        return len(self.tracks) / len(self.total_tracks) 
+
+        self.route.append(route_list)
+        self.covered_tracks = self.covered_tracks + set(route_tracks)
+        self.total_duration = self.total_duration + route_duration
+
+
+    # def covered_tracks(self): 
+    #     """
+    #     Calculates the fraction of covered tracks: p
+    #     """
+    #     return len(self.total_tracks) / len(self.total_tracks) 
         
 
     def quality(self):
+        """"
+        Calculates the quality of the network, according to the target function, based on: 
+        -fraction of covered tracks
+        -ammount of routes 
+        -total duration of the network 
+        """
 
-        # self.covered_tracks = ...
+        p = len(self.covered_tracks) / len(self.total_tracks)
 
-        # method to calculate the quality of the network using the doelfunctie
-        # p = fractie bereden verbindingen --> we moeten dus ergens bijhouden hoeveel verbindingen 
-        # we hebben bereden 
-
-
-        p = self.covered_tracks
-
-        target_function = p * 10000 - (self.routes*100 + self.min) 
+        target_function = p * 10000 - (self.route_number*100 + self.total_duration) 
      
-        pass 
+        return target_function 
