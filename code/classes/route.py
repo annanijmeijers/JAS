07712e7370eee330c66b510f91277eca779b0da7
sebastiaan-ai-23 <sebassmash08@gmail.com>
2023-06-13 15:07:38 +0200

    made the route.py file adaptive

--- code/classes/route.py
+++ code/classes/route.py
@@ -1,7 +1,5 @@
 from .station import Station 
-import random
 import copy
-
 
 class Route(): 
 
@@ -15,15 +13,14 @@ class Route():
         self.route = list()
         self.connection_list = set()
 
-    def initialise_route(self): 
+    def add_station(self, station_obj): 
         # get a starting station from dataframe
         # can be random or picked with a specific constraint
         
-        starting_station = random.choice(self.list_of_stations)
-        self.route.append(starting_station)
+        self.route.append(station_obj)
 
-    def add_connection(self): 
-
+    def check_connection(self):
+        
         # create instance for current station
         current_station = self.route[-1] # chooses the last station in the route station objects
 
@@ -39,25 +36,21 @@ class Route():
         if connections == {}:
             connections = current_station.connections
 
-        random_connection = random.choice(list(connections.keys())) # picks a random dict key as connection
-
+        return connections
+    
+    def add_connection(self, connection_key, connections): 
         # increase duration of route and decrease the total timeframe
-        self.duration += connections[random_connection]
-        self.timeframe -= connections[random_connection]
+        self.duration += connections[connection_key]
+        self.timeframe -= connections[connection_key]
 
+        stations_list = set(self.list_of_stations)
         for station in self.list_of_stations: # is dit handig via een set? Dit is niet super efficient
             
-            # searches for the station object of the random connection station
-            if station.name == random_connection: 
-                self.route.append(station)
+            # searches for the station object of the connection station
+            if station.name == connection_key: 
+                self.add_station(station)
 
     def compute_route(self):
-        #  makes a route within the timeframe
-        self.initialise_route()
-
-        while self.timeframe > 0: # checken of dit niet aan t begin moet
-            self.add_connection()
-
         # compute unique connections
         for i in range(len(self.route)):
             if i < (len(self.route) - 1):
