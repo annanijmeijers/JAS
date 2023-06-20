cacb13f01c82f8826061b7a5d6d795d2f242cb4f
annanijmeijers <anna.nijmeijers@outlook.com>
2023-06-20 11:00:19 +0200

    greedy body created

---
+++ code/algorithms/greedy.py
@@ -0,0 +1,19 @@
+import copy 
+import random 
+
+
+class Greedy(): 
+    """ 
+    The Greedy class assigns the best possible value 
+    to each station one by one. 
+
+    IN: 
+    - connection_list 
+    - station_list
+
+    OUT: 
+    - network
+
+    """
+
+    def __init__(self, )
