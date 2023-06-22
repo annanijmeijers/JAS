1c955baafceed5ac857906d8fcca77324b349281
Jensvanderweide <jensvanderweide1@gmail.com>
2023-06-22 19:45:03 +0200

    Created a stochastic version of hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -13,9 +13,12 @@ class RailClimber():
     def __init__(self, network_object, all_stations):
         self.all_stations = all_stations 
         self.network = copy.deepcopy(network_object) 
-        
-
+        self.qualities_for_vis = None
+        self.improvements = None 
+    
     def run(self, tries_per_route=100):
+
+        self.qualities_for_vis = []
 
         # shuffle the iterations to choose the route to change randomly 
         random_list = list(range(1, len(self.network.routes)+1))
@@ -37,6 +40,7 @@ class RailClimber():
                 # replace the route and calculate the new quality 
                 self.network.replace_route(r, new_route)
                 new_quality = self.network.quality()
+                self.qualities_for_vis.append(best_quality)
 
                 # check if the quality is better 
                 if new_quality >= best_quality:
@@ -47,6 +51,61 @@ class RailClimber():
                     self.network.replace_route(r, old_route)
 
         return self.network
+
+class StochasticClimber(RailClimber): 
+
+    def run(self, max_cycles): 
+
+        old_score = self.network.quality()
+        stop_trying = False
+        cycle_count = 0 
+        self.improvements = []
+        self.qualities_for_vis = []
+        pbar = tqdm()
+
+        while not stop_trying: 
+
+            # choose a random route to change 
+            route_to_change = random.choice(self.network.routes)
+            number_of_route = route_to_change.number 
+
+            # create a new route to swap 
+            new_route = Route(180, self.all_stations) 
+            RandomRoute(new_route).build_route()
+
+            # replace the route and calculate the new quality 
+            self.network.replace_route(number_of_route, new_route)
+            new_score = self.network.quality()
+            
+
+            improvement = new_score - old_score
+            self.improvements.append(improvement)
+
+            if improvement < 0:
+
+                # put the original route back
+                self.network.replace_route(number_of_route, route_to_change)
+
+            else: 
+                old_score = new_score
+
+            self.qualities_for_vis.append(old_score)   
+                
+            cycle_count += 1 
+            if cycle_count == max_cycles: 
+                stop_trying = True 
+            
+            pbar.update(1)
+
+        pbar.close
+            
+            
+                
+            
+
+
+
+
 
 
 
