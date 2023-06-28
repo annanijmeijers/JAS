d5322e0a3b9f0fe3bb42ca065fb497eb5cfd6434
Jensvanderweide <jensvanderweide1@gmail.com>
2023-06-28 10:35:06 +0200

    slight adjustments to funcs and main

--- experiments/greedyannealing_experiment.py
+++ experiments/greedyannealing_experiment.py
@@ -4,11 +4,11 @@ from code.algorithms.randomised import RandomNet
 from code.algorithms.simulatedannealing import GreedyAnnealing
 import matplotlib.pyplot as plt 
 
-def greedy_annealing(random_network, all_stations, heuristic, iterations=1000, temperature=1000): 
+def greedy_annealing(file, random_network, all_stations, heuristic, iterations=1000, temperature=1000): 
 
     ga = GreedyAnnealing(random_network, all_stations, temperature, heuristic)
 
-    with open('results/greedyannealing/greedyannealing.csv', 'w', newline='') as f: 
+    with open(f'results/greedyannealing/greedyannealing_{file}.csv', 'w', newline='') as f: 
         writer = csv.writer(f, delimiter=',')
 
         for i in tqdm(range(iterations), desc='Greedy Annealing:'): 
@@ -18,7 +18,7 @@ def greedy_annealing(random_network, all_stations, heuristic, iterations=1000, t
 
 # ====================== Comparing different numbers of routes ======================
 
-def greedy_anneal_compare_routes(network_object, all_stations, ammount_of_routes, heuristic, iterations=1000, temperature=1000):
+def greedy_anneal_compare_routes(file, network_object, all_stations, ammount_of_routes, heuristic, iterations=1000, temperature=1000):
 
     for r in range(1, ammount_of_routes+1): 
 
@@ -28,19 +28,19 @@ def greedy_anneal_compare_routes(network_object, all_stations, ammount_of_routes
 
         ga = GreedyAnnealing(random_network, all_stations, temperature, heuristic)
 
-        with open(f'results/greedyannealing/compare_routes/ga_{r}_routes.csv', 'w', newline='') as f:
+        with open(f'results/greedyannealing/compare_routes/ga_{r}_routes_{file}.csv', 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
 
             for i in tqdm(range(iterations), desc=f'Greedy Annealing, routes{r}:', leave=False): 
                 ga.run(1)
                 writer.writerow([ga.network.quality()])
 
-def plot_ga_compare_routes(amount_of_routes, highest_values_only): 
+def plot_ga_compare_routes(file, amount_of_routes, highest_values_only): 
 
     fig, ax = plt.subplots()
 
     for r in range(1, amount_of_routes+1):
-        with open(f'results/greedyannealing/compare_routes/ga_{r}_routes.csv', 'r') as f:
+        with open(f'results/greedyannealing/compare_routes/ga_{r}_route{file}.csv', 'r') as f:
             reader = csv.reader(f, delimiter=',')
             values = list(reader)
             values = [float(k) for sublist in values for k in sublist]
@@ -70,30 +70,30 @@ def plot_ga_compare_routes(amount_of_routes, highest_values_only):
 
 # ====================== Comparing different temperatures ======================
 
-def greedy_anneal_compare_temps(random_network, all_stations, heuristic, iterations=1000, temperature=1000):
+def greedy_anneal_compare_temps(file, random_network, all_stations, heuristic, iterations=1000, temperature=1000):
 
     for temp in range(100, temperature+100, 100): 
 
         ga = GreedyAnnealing(random_network, all_stations, temp, heuristic)
 
-        with open(f'results/greedyannealing/compare_temps/ga_temp{temp}.csv', 'w', newline='') as f:
+        with open(f'results/greedyannealing/compare_temps/ga_temp{temp}_{file}.csv', 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
 
             for i in tqdm(range(iterations), desc=f'Greedy Annealing, temp{temp}:', leave=False): 
                 ga.run(1)
                 writer.writerow([ga.network.quality()])
 
-def plot_ga_compare_temps(temperature): 
+def plot_ga_compare_temps(file, temperature): 
 
     fig, ax = plt.subplots()
     cmap = plt.colormaps["plasma"]
 
     for temp in range(100, temperature+100, 100):
-        with open(f'results/greedyannealing/compare_temps/ga_temp{temp}.csv', 'r') as f:
+        with open(f'results/greedyannealing/compare_temps/ga_temp{temp}_{file}.csv', 'r') as f:
             reader = csv.reader(f, delimiter=',')
             values = list(reader)
             values = [float(k) for sublist in values for k in sublist]
-            ax.plot(values, label=f'Temp: {temp}', color=cmap(temp))
+            ax.plot(values, label=f'Temp: {temp}', color=cmap(temp/1100))
     
     
     ax.legend(loc='lower right')
